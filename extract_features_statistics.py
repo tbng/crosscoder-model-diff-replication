8916607c75a6b8b321495bf7e4a71ef0185676aa
tbng <tuanbinhs@gmail.com>
2025-02-20 19:53:20 +0700

    hack sae vis to have attention mask

--- extract_features_statistics.py
+++ extract_features_statistics.py
@@ -1,9 +1,11 @@
+from datetime import datetime
+
 import copy
 import plotly.express as px
 import torch
 import torch.nn.functional as F
 from transformer_lens import HookedTransformer
-
+from sae_vis.data_config_classes import SaeVisConfig
 from crosscoder import CrossCoder
 from utils import get_gsm8k_dataset
 from datasets import load_dataset
@@ -27,8 +29,8 @@ math_model = HookedTransformer.from_pretrained(
 )
 
 
-version = 'version_10'
-ckpt = 3
+version = 'version_4'
+ckpt = 6
 
 cross_coder = CrossCoder.load(version, ckpt)
 collect_layer = 14
@@ -36,7 +38,7 @@ collect_layer = 14
 train_questions, train_answers = get_gsm8k_dataset(split='train')
 
 
-# Load dataset 
+# Load dataset
 
 eos = base_model.tokenizer.special_tokens_map['eos_token']
 train_questions, train_answers = get_gsm8k_dataset(split='train')
@@ -44,7 +46,8 @@ merged_prompts = [
     f"Problem: {q} {eos} Answer: {a}" for q, a in zip(train_questions, train_answers)
 ]
 
-data_prm800k = load_dataset('json', data_files='data/prm800k/prm800k_train.jsonl')['train']
+data_prm800k = load_dataset(
+    'json', data_files='data/prm800k/prm800k_train.jsonl')['train']
 problem, solution, answer = data_prm800k['problem'], data_prm800k['solution'], data_prm800k['answer']
 eos = base_model.tokenizer.special_tokens_map['eos_token']
 
@@ -71,27 +74,40 @@ base_estimated_scaling_factor, math_estimated_scaling_factor = 0.7367579, 0.3833
 
 folded_cross_coder = copy.deepcopy(cross_coder)
 
-# we'll only fold the normalization scaling factors into W_enc, since we aren't splicing back into the model with the reconstructed 
+# we'll only fold the normalization scaling factors into W_enc, since we aren't splicing back into the model with the reconstructed
+
+
 def fold_activation_scaling_factor(cross_coder, base_scaling_factor, chat_scaling_factor):
-    cross_coder.W_enc.data[0, :, :] = cross_coder.W_enc.data[0, :, :] * base_scaling_factor
-    cross_coder.W_enc.data[1, :, :] = cross_coder.W_enc.data[1, :, :] * chat_scaling_factor
+    cross_coder.W_enc.data[0, :, :] = cross_coder.W_enc.data[0,
+                                                             :, :] * base_scaling_factor
+    cross_coder.W_enc.data[1, :, :] = cross_coder.W_enc.data[1,
+                                                             :, :] * chat_scaling_factor
 
     return cross_coder
 
-folded_cross_coder = fold_activation_scaling_factor(folded_cross_coder, base_estimated_scaling_factor, math_estimated_scaling_factor)
 
-from sae_vis.model_fns import CrossCoderConfig, CrossCoder
-from sae_vis.data_config_classes import SaeVisConfig
+def get_sae_vis_cross_coder(folded_cross_coder):
 
-encoder_cfg = CrossCoderConfig(d_in=base_model.cfg.d_model, d_hidden=cross_coder.cfg["dict_size"], apply_b_dec_to_input=False)
-sae_vis_cross_coder = CrossCoder(encoder_cfg)
-sae_vis_cross_coder.load_state_dict(folded_cross_coder.state_dict())
-sae_vis_cross_coder = sae_vis_cross_coder.to(device)
-sae_vis_cross_coder = sae_vis_cross_coder.to(torch.bfloat16)
+    from sae_vis.model_fns import CrossCoderConfig, CrossCoder
+
+    encoder_cfg = CrossCoderConfig(d_in=base_model.cfg.d_model,
+                                   d_hidden=cross_coder.cfg["dict_size"],
+                                   apply_b_dec_to_input=False)
+    sae_vis_cross_coder = CrossCoder(encoder_cfg)
+    sae_vis_cross_coder.load_state_dict(folded_cross_coder.state_dict())
+    sae_vis_cross_coder = sae_vis_cross_coder.to(device)
+    sae_vis_cross_coder = sae_vis_cross_coder.to(torch.bfloat16)
+    return sae_vis_cross_coder
+
+
+folded_cross_coder = fold_activation_scaling_factor(
+    folded_cross_coder, base_estimated_scaling_factor, math_estimated_scaling_factor)
+sae_vis_cross_coder = get_sae_vis_cross_coder(folded_cross_coder)
 
 distinct_latent_mask = (relative_norms > 0.95).cpu()
 torch.sum(distinct_latent_mask)
-distinct_feature_idx = torch.arange(cross_coder.cfg['dict_size'])[distinct_latent_mask]
+distinct_feature_idx = torch.arange(cross_coder.cfg['dict_size'])[
+    distinct_latent_mask]
 
 sae_vis_config = SaeVisConfig(
     hook_point=folded_cross_coder.cfg["hook_point"],
@@ -104,26 +120,28 @@ sae_vis_config = SaeVisConfig(
 print(len(distinct_feature_idx))
 
 
-num_samples = 10
-perm_idx = torch.randperm(len(all_tokens_padded.input_ids))[:num_samples].cpu().numpy()
+num_samples = 100
+perm_idx = torch.randperm(len(all_tokens_padded.input_ids))[
+    :num_samples].cpu().numpy()
 
 sae_vis_data = SaeVisData.create(
-    encoder = sae_vis_cross_coder,
-    encoder_B = None,
-    model_A = base_model,
-    model_B = math_model,
-    tokens = all_tokens_padded.input_ids[perm_idx], # in practice, better to use more data
-    cfg = sae_vis_config,
+    encoder=sae_vis_cross_coder,
+    encoder_B=None,
+    model_A=base_model,
+    model_B=math_model,
+    # in practice, better to use more data
+    tokens=all_tokens_padded.input_ids[perm_idx],
+    attention_mask=all_tokens_padded.attention_mask[perm_idx],
+    cfg=sae_vis_config,
 )
 
 # Save visualization
-from datetime import datetime
 
 current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
 filename = f"crosscoder_{current_time}_{version}_{ckpt}_feature_vis_demo.html"
 promptvis = f"crosscoder_{current_time}_{version}_{ckpt}_prompt_vis_demo.html"
-
 sae_vis_data.save_feature_centric_vis(filename)
-sae_vis_data.save_prompt_centric_vis(combined_prompts[perm_idx], promptvis)
+
+# sae_vis_data.save_prompt_centric_vis(combined_prompts[perm_idx], promptvis)
 
 print('Done!')
---
+++ sae_vis/model_fns.py
@@ -0,0 +1,221 @@
+import re
+from dataclasses import dataclass
+from typing import Literal, overload
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from jaxtyping import Float, Int
+from torch import Tensor
+from transformer_lens import HookedTransformer, utils
+from transformer_lens.hook_points import HookPoint
+import einops
+
+DTYPES = {"fp32": torch.float32, "fp16": torch.float16, "bf16": torch.bfloat16}
+
+@dataclass
+class CrossCoderConfig:
+    """Class for storing configuration parameters for the CrossCoder"""
+
+    d_in: int
+    d_hidden: int | None = None
+    dict_mult: int | None = None
+
+    l1_coeff: float = 3e-4
+
+    apply_b_dec_to_input: bool = False
+
+    def __post_init__(self):
+        assert (
+            int(self.d_hidden is None) + int(self.dict_mult is None) == 1
+        ), "Exactly one of d_hidden or dict_mult must be provided"
+        if (self.d_hidden is None) and isinstance(self.dict_mult, int):
+            self.d_hidden = self.d_in * self.dict_mult
+        elif (self.dict_mult is None) and isinstance(self.d_hidden, int):
+            #assert self.d_hidden % self.d_in == 0, "d_hidden must be a multiple of d_in"
+            self.dict_mult = self.d_hidden // self.d_in
+
+
+class CrossCoder(nn.Module):
+    def __init__(self, cfg: CrossCoderConfig):
+        super().__init__()
+        self.cfg = cfg
+
+        assert isinstance(cfg.d_hidden, int)
+
+        # W_enc has shape (d_in, d_encoder), where d_encoder is a multiple of d_in (cause dictionary learning; overcomplete basis)
+        self.W_enc = nn.Parameter(
+            torch.nn.init.kaiming_uniform_(torch.empty(2, cfg.d_in, cfg.d_hidden))
+        )
+        self.W_dec = nn.Parameter(
+            torch.nn.init.kaiming_uniform_(torch.empty(cfg.d_hidden, 2, cfg.d_in))
+        )
+        self.b_enc = nn.Parameter(torch.zeros(cfg.d_hidden))
+        self.b_dec = nn.Parameter(torch.zeros(2, cfg.d_in))
+        self.W_dec.data[:] = self.W_dec / self.W_dec.norm(dim=-1, keepdim=True)
+
+    def forward(self, x: torch.Tensor):
+        # TODO: lots of this stuff is legacy SAE stuff that probably is wrong / unnecessary
+        x_cent = x - self.b_dec * self.cfg.apply_b_dec_to_input
+        x_enc = einops.einsum(
+            x_cent,
+            self.W_enc,
+            "... n_layers d_model, n_layers d_model d_hidden -> ... d_hidden",
+        )
+        acts = F.relu(x_enc + self.b_enc)
+        #x_reconstruct = acts @ self.W_dec + self.b_dec
+        x_reconstruct = einops.einsum(
+            acts,
+            self.W_dec,
+            "... d_hidden, d_hidden n_layers d_model -> ... n_layers d_model",
+        )
+        diff = x_reconstruct.float() - x.float()
+        squared_diff = diff.pow(2)
+        l2_per_batch = einops.reduce(squared_diff, 'batch n_layers d_model -> batch', 'sum')
+        l2_loss = l2_per_batch.mean()
+        #l2_loss = (x_reconstruct.float() - x.float()).pow(2).sum(-1).mean(0)
+        decoder_norms = self.W_dec.norm(dim=-1)
+        # decoder_norms: [d_hidden, n_layers]
+        total_decoder_norm = einops.reduce(decoder_norms, 'd_hidden n_layers -> d_hidden', 'sum')
+        l1_loss = (acts * total_decoder_norm[None, :]).sum(-1).mean(0)
+        loss = l2_loss + l1_loss
+        return loss, x_reconstruct, acts, l2_loss, l1_loss
+
+    @torch.no_grad()
+    def remove_parallel_component_of_grads(self):
+        W_dec_normed = self.W_dec / self.W_dec.norm(dim=-1, keepdim=True)
+        W_dec_grad_proj = (self.W_dec.grad * W_dec_normed).sum(
+            -1, keepdim=True
+        ) * W_dec_normed
+        self.W_dec.grad -= W_dec_grad_proj
+
+    def __repr__(self) -> str:
+        return f"CrossCoder(d_in={self.cfg.d_in}, dict_mult={self.cfg.dict_mult})"
+
+
+# # ==============================================================
+# # ! TRANSFORMERS
+# # This returns the activations & resid_pre as well (optionally)
+# # ==============================================================
+
+
+class TransformerLensWrapper(nn.Module):
+    """
+    This class wraps around & extends the TransformerLens model, so that we can make sure things like the forward
+    function have a standardized signature.
+    """
+
+    def __init__(self, model: HookedTransformer, hook_point: str):
+        super().__init__()
+        assert (
+            hook_point in model.hook_dict
+        ), f"Error: hook_point={hook_point!r} must be in model.hook_dict"
+        self.model = model
+        self.hook_point = hook_point
+
+        # Get the layer (so we can do the early stopping in our forward pass)
+        layer_match = re.match(r"blocks\.(\d+)\.", hook_point)
+        assert layer_match, f"Error: expecting hook_point to be 'blocks.{{layer}}.{{...}}', but got {hook_point!r}"
+        self.hook_layer = int(layer_match.group(1))
+
+        # Get the hook names for the residual stream (final) and residual stream (immediately after hook_point)
+        self.hook_point_resid = utils.get_act_name("resid_post", self.hook_layer)
+        self.hook_point_resid_final = utils.get_act_name(
+            "resid_post", self.model.cfg.n_layers - 1
+        )
+        assert self.hook_point_resid in model.hook_dict
+        assert self.hook_point_resid_final in model.hook_dict
+
+    @overload
+    def forward(
+        self,
+        tokens: Tensor,
+        return_logits: Literal[True],
+    ) -> tuple[Tensor, Tensor, Tensor]: ...
+
+    @overload
+    def forward(
+        self,
+        tokens: Tensor,
+        return_logits: Literal[False],
+    ) -> tuple[Tensor, Tensor]: ...
+
+    def forward(
+        self,
+        tokens: Int[Tensor, "batch seq"],
+        attention_mask: Tensor,
+        return_logits: bool = True,
+    ):
+        """
+        Inputs:
+            tokens: Int[Tensor, "batch seq"]
+                The input tokens, shape (batch, seq)
+            return_logits: bool
+                If True, returns (logits, residual, activation)
+                If False, returns (residual, activation)
+        """
+
+        # Run with hook functions to store the activations & final value of residual stream
+        # If return_logits is False, then we compute the last residual stream value but not the logits
+        output: Tensor = self.model.run_with_hooks(
+            tokens,
+            attention_mask=attention_mask,
+            # stop_at_layer = (None if return_logits else self.hook_layer),
+            fwd_hooks=[
+                (self.hook_point, self.hook_fn_store_act),
+                (self.hook_point_resid_final, self.hook_fn_store_act),
+            ],
+        )
+
+        # The hook functions work by storing data in model's hook context, so we pop them back out
+        activation: Tensor = self.model.hook_dict[self.hook_point].ctx.pop("activation")
+        if self.hook_point_resid_final == self.hook_point:
+            residual: Tensor = activation
+        else:
+            residual: Tensor = self.model.hook_dict[
+                self.hook_point_resid_final
+            ].ctx.pop("activation")
+
+        if return_logits:
+            return output, residual, activation
+        return residual, activation
+
+    def hook_fn_store_act(self, activation: torch.Tensor, hook: HookPoint):
+        hook.ctx["activation"] = activation
+
+    @property
+    def tokenizer(self):
+        return self.model.tokenizer
+
+    @property
+    def W_U(self):
+        return self.model.W_U
+
+    @property
+    def W_out(self):
+        return self.model.W_out
+
+
+def to_resid_dir(dir: Float[Tensor, "feats d_in"], model: TransformerLensWrapper):
+    """
+    Takes a direction (eg. in the post-ReLU MLP activations) and returns the corresponding dir in the residual stream.
+
+    Args:
+        dir:
+            The direction in the activations, i.e. shape (feats, d_in) where d_in could be d_model, d_mlp, etc.
+        model:
+            The model, which should be a HookedTransformerWrapper or similar.
+    """
+    # If this SAE was trained on the residual stream or attn/mlp out, then we don't need to do anything
+    if "resid" in model.hook_point or "_out" in model.hook_point:
+        return dir
+
+    # If it was trained on the MLP layer, then we apply the W_out map
+    elif ("pre" in model.hook_point) or ("post" in model.hook_point):
+        return dir @ model.W_out[model.hook_layer]
+
+    # Others not yet supported
+    else:
+        raise NotImplementedError(
+            "The hook your SAE was trained on isn't yet supported"
+        )
